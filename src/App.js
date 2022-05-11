c384a1116c7d21cea2fe50d34efc8dc706966b00
Sai Bingi <saibingi1208@gmail.com>
2022-05-11 09:07:20 +0000

    Digital Timer App Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Published URL  ==>  https://saidigitaltimer.ccbp.tech
+
+import DigitalTimer from './components/DigitalTimer'
+
+import './App.css'
+
+const App = () => <DigitalTimer />
+
+export default App
